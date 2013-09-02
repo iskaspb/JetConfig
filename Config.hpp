292cc79726adc02fe69b572c7b33641d4ce36bd8
iskaspb <iskaspb@gmail.com>
2013-09-03 07:43:28 +0800

    enrich Config interface with more methods (to be implemented)

--- Config.hpp
+++ Config.hpp
@@ -12,6 +12,7 @@
 #include "ConfigSource.hpp"
 #include <boost/lexical_cast.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
 
 namespace jet
 {
@@ -29,17 +30,36 @@ public:
     const Config& operator=(const Config& other);
     ~Config();
 
+    Config& operator<<(const ConfigSource& source);
+    void operator<<(ConfigLock);
+    std::string toString() const;//TODO: finish
+
     const std::string& name() const;
     const std::string& appName() const;
     const std::string& instanceName() const;
-    std::string get(const std::string& attrName) const;
+    
+    std::string get(const std::string& attrName) const;//TODO: get() without default value should raise exception
     template<typename T>
     T get(const std::string& attrName) const
-    {
+    {//TODO: translate bad_lexical_cast into ConfigError
         return boost::lexical_cast<T>(get(attrName));
     }
-    Config& operator<<(const ConfigSource& source);
-    void operator<<(ConfigLock);
+    boost::optional<std::string> getOptional(const std::string& attrName) const;
+    template<typename T>
+    boost::optional<T> getOptional(const std::string& attrName) const
+    {//TODO: translate bad_lexical_cast into ConfigError
+        const boost::optional<std::string> value(getOptional(attrName));
+        if(value)
+            return boost::lexical_cast<T>(*value);
+        return boost::none;
+    }
+    std::string get(const std::string& attrName, const std::string& defaultValue);//TODO: finish
+    template<typename T>
+    T get(const std::string& attrName, const T& defaultValue)
+    {
+        const std::string strDefaultValue(boost::lexical_cast<std::string>(defaultValue));//TODO: translate bad_lexical_cast into ConfigError
+        return boost::lexical_cast<T>(get(attrName, strDefaultValue));//TODO: translate bad_lexical_cast into ConfigError
+    }
 private:
     class Impl;
     boost::shared_ptr<Impl> impl_;
